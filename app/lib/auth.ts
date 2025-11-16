a7cf9b393551f3a6ac33a9d2b9c1af924dc39099
Ipule Pipi <ipulepipi60@gmail.com>
2025-11-16 12:51:53 -0500

    create better auth instance

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,5 +1,12 @@
 import { betterAuth } from "better-auth";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import clientPromise from "../lib/mongodb"; // your mongodb client
+
+const client = await clientPromise;
+const db = client.db(); // Uses default database from connection string
 
 export const auth = betterAuth({
-  //...
+    database: mongodbAdapter(db, {
+        client // Optional: enables database transactions
+    }),
 });
